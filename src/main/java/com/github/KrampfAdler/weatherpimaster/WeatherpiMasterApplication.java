30fafd9b8079de806584c30fb2eb9e05fd81e021
Pascal Harder <pascal.harder@yourstruly.de>
2019-08-13 11:22:50 +0200

    initial Commit

---
+++ src/main/java/com/github/KrampfAdler/weatherpimaster/WeatherpiMasterApplication.java
@@ -0,0 +1,13 @@
+package com.github.KrampfAdler.weatherpimaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherpiMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherpiMasterApplication.class, args);
+	}
+
+}
