ab3476db2420977ad2f5f357d2bd5cd898dbc4b8
Pascal Harder <pascal.harder@yourstruly.de>
2020-01-08 17:12:50 +0100

    adds tempsMonthly

--- src/main/java/com/github/KrampfAdler/weatherpimaster/controller/WeatherController.java
+++ src/main/java/com/github/KrampfAdler/weatherpimaster/controller/WeatherController.java
@@ -30,7 +30,7 @@ public class WeatherController {
     }
 
     @GetMapping(path = "/weather/tempsToday")
-    public String getGraph(Model model){
+    public String getTempsToday(Model model){
         List<Map<Object,Object>> tempsdataPoints1 = new ArrayList<Map<Object,Object>>();
         List<Map<Object,Object>> humititydataPoints1 = new ArrayList<Map<Object,Object>>();
         Calendar now = Calendar.getInstance();
@@ -50,6 +50,53 @@ public class WeatherController {
         model.addAttribute("tempPointsList", tempsdataPoints1);
         model.addAttribute("humidityPointsList", humititydataPoints1);
         return "tempsToday";
+    }
+
+    @GetMapping(path = "/weather/tempsMonth")
+    public String getGraph(Model model){
+        List<Map<Object,Object>> highDataPoints1 = new ArrayList<Map<Object,Object>>();
+        List<Map<Object,Object>> lowDataPoints1 = new ArrayList<Map<Object,Object>>();
+        Calendar now = Calendar.getInstance();
+        Calendar yesterday = Calendar.getInstance();
+        yesterday.add(Calendar.MONTH, -1);
+        double dailyHighest = -100;
+        double dailyLowest = 100;
+        Calendar day = Calendar.getInstance();
+        for(WeatherMesurement weatherMesurement : weatherMesurementRepository.findAllByCreatedBetween(yesterday.getTime(), now.getTime())){
+            Calendar date = Calendar.getInstance();
+            date.setTime(weatherMesurement.getCreated());
+            boolean sameDay = date.get(Calendar.DAY_OF_YEAR) == day.get(Calendar.DAY_OF_YEAR) &&
+                    date.get(Calendar.YEAR) == day.get(Calendar.YEAR);
+            if(sameDay) {
+                if(weatherMesurement.getAmbientTemperature() > dailyHighest){
+                    dailyHighest = weatherMesurement.getAmbientTemperature();
+                }
+                if(weatherMesurement.getAmbientTemperature() < dailyLowest){
+                    dailyLowest = weatherMesurement.getAmbientTemperature();
+                }
+            }else{
+                day = date;
+                if(dailyHighest != -100) {
+                    Map<Object, Object> map = new HashMap<Object, Object>();
+                    map.put("x", day.getTime());
+                    map.put("y", dailyHighest);
+                    highDataPoints1.add(map);
+                }
+                if(dailyLowest != 100) {
+                    Map<Object, Object> map2 = new HashMap<Object, Object>();
+                    map2.put("x", day.getTime());
+                    map2.put("y", dailyLowest);
+                    lowDataPoints1.add(map2);
+                }
+                dailyHighest = -100;
+                dailyLowest = 100;
+                day.setTime(weatherMesurement.getCreated());
+
+            }
+        }
+        model.addAttribute("highDataPointsList", highDataPoints1);
+        model.addAttribute("lowDataPointsList", lowDataPoints1);
+        return "tempsMonth";
     }
 
     @GetMapping(path = "/weather/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
