b2706f7651bbc5ab26c001d9a8d63b3d7aebd056
KrampfAdler <KrampfAdler@gmail.com>
2024-03-16 13:53:09 +0100

    updated dependencies

--- src/main/java/com/github/KrampfAdler/weatherpimaster/WeatherpiMasterApplication.java
+++ src/main/java/com/github/KrampfAdler/weatherpimaster/WeatherpiMasterApplication.java
@@ -1,5 +1,6 @@
 package com.github.KrampfAdler.weatherpimaster;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -8,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import javax.annotation.PostConstruct;
+
 import java.util.Date;
 import java.util.TimeZone;
 
